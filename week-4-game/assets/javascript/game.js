6f5d6e78937276dbb82770c8393de5b45a400f42
Roy Hinojosa <roy.hinojosa@gmail.com>
2016-05-21 17:01:41 -0600

    made massive changes

--- week-4-game/assets/javascript/game.js
+++ week-4-game/assets/javascript/game.js
@@ -1,55 +1,76 @@
 //variables and containers for the game
 
-var attack = 0;
-var health = 0;
-var availableCharacters = [];
-var enemies = [];
-var hasPickedCharacter = false;
+var heroAttack = 0;
+var heroHealth = 0;
+var attackCounter = 0;
+var villainAttack = 0;
+var villainHealth = 0;
+var heroClicked = false;
 
-$( "#cartman" ).data( "cartman", {divId: "cartman", heroName: "Cartman", hp: 180, ap: 15, cp: 15} );
-$( "#kenny" ).data( "kenny", {divId: "kenny", heroName: "Kenny", hp: 80, ap: 30, cp: 20} );
-$( "#mrhat" ).data( "mrhat", {divId: "mrhat", heroName: "Mr. Hat", hp: 150, ap: 10, cp: 9} );
-$( "#randy" ).data( "randy", {divId: "randy", heroName: "Randy", hp: 120, ap: 8, cp: 12} );
+$( "#cartman" ).data({divId: "cartman", heroName: "Cartman", hp: 180, ap: 15, cp: 15} );
+$( "#kenny" ).data({divId: "kenny", heroName: "Kenny", hp: 80, ap: 30, cp: 20} );
+$( "#mrhat" ).data({divId: "mrhat", heroName: "Mr. Hat", hp: 150, ap: 10, cp: 9} );
+$( "#randy" ).data({divId: "randy", heroName: "Randy", hp: 120, ap: 8, cp: 12} );
 
 //functions for game
 function hpDisplay($id,$hp) {
-	return $($id).append( "<p>" + $hp + "</p>");
+	return $($id).html( "<p>" + $hp + "</p>");
 }
 
 function heroNameDisplay($id,$heroName) {
-	return $($id).prepend( "<p>" + $heroName + "</p>");
+	return $($id).html( "<p>" + $heroName + "</p>");
 }
+
+function updateValues() {
+	var cartmanData = $( "#cartman" ).data();
+	hpDisplay("#cartmanHp", cartmanData.hp);
+
+	var kennyData = $( "#kenny" ).data();
+	hpDisplay("#kennyHp", kennyData.hp);
+
+	var mrhatData = $( "#mrhat" ).data();
+	hpDisplay("#mrhatHp", mrhatData.hp);
+
+	var randyData = $( "#randy" ).data();
+	hpDisplay("#randyHp", randyData.hp);
+}
+
 
 //jquery functions for click events and assigning
 $( document ).ready(function() {
+	updateValues();
 
-	var cartmanData = $( "#cartman" ).data();
-	availableCharacters.push(cartmanData);
-	heroNameDisplay("#cartman", cartmanData.cartman.heroName);
-	hpDisplay("#cartman", cartmanData.cartman.hp);
+	var heroData, enemyData;
 
-	var kennyData = $( "#kenny" ).data();
-	availableCharacters.push(kennyData);
-	heroNameDisplay("#kenny", kennyData.kenny.heroName);
-	hpDisplay("#kenny", kennyData.kenny.hp);
+	var captureHeroImage;
+	$(".character").on("click", function() {
+		// this is the elment that got clicked
 
-	var mrhatData = $( "#mrhat" ).data();
-	availableCharacters.push(mrhatData);
-	heroNameDisplay("#mrhat", mrhatData.mrhat.heroName);
-	hpDisplay("#mrhat", mrhatData.mrhat.hp);
+		if (heroClicked) { 
+			// attach to villain 
+			$("#defender").append(this);
+			enemyData = $(this).data();
+			if (enemyData.hp <= 0) {
+				$(this).remove();
+			}
 
-	var randyData = $( "#randy" ).data();
-	availableCharacters.push(randyData);
-	heroNameDisplay("#randy", randyData.randy.heroName);
-	hpDisplay("#randy", randyData.randy.hp);
+		} else { 
+			// attach to hero 
+			$("#pickedCharacter").append(this);
+			$(".characterList .character").addClass("villains");
+			heroData = $(this).data();
+			heroClicked = true;
+			attackCounter = heroData.ap;
+		}
+		updateValues();
 
-	var captureImage;
-	$(".image").on('click', function() {
-		captureImage = $(this).detach();
-		$(this).addClass("hero");
-		(captureImage).appendTo("#pickedCharacter");
-		$(".hero").off("click");
-		$(".image").siblings().addClass("villains");
-		$(".villains").detach().appendTo("#enemies");		
+	});
+
+	$("#attack").on("click", function() {
+		heroData.hp -= enemyData.cp;
+		enemyData.hp -= heroData.ap;
+		heroData.ap += attackCounter;
+		updateValues();
+
 	});
 });
